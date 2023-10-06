d662811e186efa993591568ed9c80818a78c7630
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-10-06 14:03:33 -0700

    update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', go)
 window.addEventListener('resize', go)
 
 function go(e) {
-    let scrollPos = document.querySelector('main').scrollTo*1.5
-    let mainHeight = document.querySelector('main').scrollTo*1.5
+    let scrollPos = document.querySelector('main').scrollTop*1.5
+    let mainHeight = document.querySelector('main').clientHeight
     document.querySelector('img').style.rotate = `${(scrollPos/mainHeight)*360}deg`
 }
